3dd262319a7cb99ba7ccdacbb3b40b6572f8fc5c
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-05-19 17:25:48 -0300

    Add factorial method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,9 @@
 class Solver
+  def factorial(num)
+    raise ArgumentError, 'Wrong type of argument' if !num.is_a?(Integer) || num.negative?
 
+    return 1 if num.zero?
+
+    num * (factorial num - 1)
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -12,13 +12,13 @@ describe Solver do
 
   describe '#factorial' do
     it 'Should return the result' do
-      expect(@solver.factorial(2)).to eql(2)
+      expect(@solver.factorial(4)).to eql(24)
     end
     it 'Should return one if the parameter is zero' do
       expect(@solver.factorial(0)).to eql(1)
     end
-    it 'Shoud raise an exception if is not a positive number' do
-      expect { @solver.factorial(-2) }.to raise_error WrongTypeOfArgument
+    it 'Should raise an exception if is not a positive number' do
+      expect { Solver.new.factorial(-2) }.to raise_error(ArgumentError)
     end
   end
 end
