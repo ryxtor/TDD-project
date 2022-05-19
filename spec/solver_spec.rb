c1d2c84f68cbf4bb30db784b21fef485283696eb
ryxtor <57068560+ryxtor@users.noreply.github.com>
2022-05-19 17:07:38 -0300

    Add tests for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,4 +9,16 @@ describe Solver do
       expect(@solver).to be_an_instance_of Solver
     end
   end
+
+  describe '#factorial' do
+    it 'Should return the result' do
+      expect(@solver.factorial(2)).to eql(2)
+    end
+    it 'Should return one if the parameter is zero' do
+      expect(@solver.factorial(0)).to eql(1)
+    end
+    it 'Shoud raise an exception if is not a positive number' do
+      expect { @solver.factorial(-2) }.to raise_error WrongTypeOfArgument
+    end
+  end
 end
